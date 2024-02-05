f756177ca6b5cd4ce28845b9b8b5de4039e009b7
lyrahgames <lyrahgames@mailbox.org>
2024-02-05 16:50:42 +0100

    Add function `string_from_file`

--- libensketch-xstd/sources/ensketch/xstd/utility.hpp
+++ libensketch-xstd/sources/ensketch/xstd/utility.hpp
@@ -25,10 +25,21 @@
 #include <typeinfo>
 
 // We will make use of exceptions to handle errors.
-// For that, include strings and the standard exceptions.
+// For that, include the standard exceptions.
 //
 #include <stdexcept>
+
+// Utilities to use, manipulate, and format
+// strings are always needed.
+//
+#include <format>
 #include <string>
+
+// Almost all executables and libraries will provide interactions
+// with the filesystem and reading from or writing to files.
+//
+#include <filesystem>
+#include <fstream>
 
 namespace ensketch::xstd {
 
@@ -59,6 +70,8 @@ using uint64 = uint64_t;
 //
 using zstring  = char*;
 using czstring = const char*;
+
+using std::string;
 
 // The 'meta' namespace is meant for so-called meta functions.
 // Meta functions are functions that work on types
@@ -157,5 +170,33 @@ namespace instance {
 using namespace meta;
 
 }  // namespace instance
+
+/// Returns the content of a file given by its path as a standard `string` object.
+///
+inline auto string_from_file(const std::filesystem::path& path) -> string {
+  using namespace std;
+
+  // We will read all characters as block and open the file in binary mode.
+  // Make sure to jump to its end for directly reading its size.
+  //
+  ifstream file{path, ios::binary | ios::ate};
+  if (!file)
+    throw runtime_error(format("Failed to open file '{}'.", path.string()));
+
+  // Read the file's size.
+  //
+  auto size = file.tellg();
+
+  // Prepare the result string with a sufficiently large buffer.
+  //
+  string result(size, '\0');
+
+  // Go back to the start and read all characters at once in a block.
+  //
+  file.seekg(0);
+  file.read(result.data(), size);
+
+  return result;
+};
 
 }  // namespace ensketch::xstd
