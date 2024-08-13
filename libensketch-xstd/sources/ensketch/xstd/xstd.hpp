2e43f90e1c7daec26cd4c0e891036a66293fc433
lyrahgames <lyrahgames@mailbox.org>
2024-08-13 20:16:38 +0200

    Add amalgamation header `xstd.hpp`

---
+++ libensketch-xstd/sources/ensketch/xstd/xstd.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <ensketch/xstd/static_identifier_list.hpp>
+#include <ensketch/xstd/static_index_list.hpp>
+#include <ensketch/xstd/static_radix_tree.hpp>
+#include <ensketch/xstd/static_zstring.hpp>
+#include <ensketch/xstd/static_zstring_list.hpp>
+#include <ensketch/xstd/type_list.hpp>
+#include <ensketch/xstd/utility.hpp>
+#include <ensketch/xstd/value_list.hpp>
+#include <ensketch/xstd/version.hpp>
---
+++ libensketch-xstd/tests/basics/xstd.cpp
@@ -0,0 +1,1 @@
+#include <ensketch/xstd/xstd.hpp>
