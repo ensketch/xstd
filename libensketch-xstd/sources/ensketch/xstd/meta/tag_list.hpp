1533d352999407fbccc2f8395c3666a0a3a88571
lyrahgames <lyrahgames@mailbox.org>
2024-08-21 00:03:22 +0200

    Remove `tag_list.hpp`

--- libensketch-xstd/sources/ensketch/xstd/meta/tag_list.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <ensketch/xstd/meta/type_list.hpp>
-
-namespace ensketch::xstd::meta {
-
-template <generic::tag... types>
-using tag_list = type_list<types...>;
-
-}  // namespace ensketch::xstd::meta
