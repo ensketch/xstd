ca4865da66fbff0d7bb3942d1bd2bf4201b7cc35
lyrahgames <lyrahgames@mailbox.org>
2024-08-22 18:35:41 +0200

    Transfer `xstd/meta/meta.hpp` to `xstd/meta.hpp`

--- libensketch-xstd/sources/ensketch/xstd/utility.hpp
+++ libensketch-xstd/sources/ensketch/xstd/utility.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <ensketch/xstd/meta/meta.hpp>
+#include <ensketch/xstd/meta.hpp>
 
 namespace ensketch::xstd {
 
--- libensketch-xstd/sources/ensketch/xstd/xstd.hpp
+++ libensketch-xstd/sources/ensketch/xstd/xstd.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <ensketch/xstd/io.hpp>
-#include <ensketch/xstd/meta/meta.hpp>
+#include <ensketch/xstd/match.hpp>
+#include <ensketch/xstd/meta.hpp>
 #include <ensketch/xstd/utility.hpp>
 #include <ensketch/xstd/version.hpp>
